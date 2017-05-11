abe1a261af114da1fa3fd97ddf00bd5a1c7b1c28
David Hazeland <davidhazeland@gmail.com>
2017-05-11 21:21:46 +0700

    Implement utils

---
+++ src/utils/__tests__/api.js
@@ -0,0 +1,14 @@
+import test from 'tape'
+import { create } from '../api'
+
+test('request() function', assert => {
+  const api = create({
+    baseURL: 'https://httpbin.org',
+    token: 'token'
+  })
+  api.get('/get').then(response => {
+    assert.deepEqual(response.status, 200,
+      'should be request success')
+    assert.end()
+  })
+})
--- src/utils/__tests__/validation.js
+++ src/utils/__tests__/validation.js
@@ -1,7 +1,16 @@
 import test from 'tape';
 import {required} from '../validation';
 
-test('required', assert => {
-  assert.deepEqual(required(1), undefined);
-  assert.end();
+test('required', nest => {
+  nest.test('pass', assert => {
+    assert.deepEqual(required(1), undefined)
+    assert.end()
+  })
+})
+
+test('required', nest => {
+  nest.test('pass', assert => {
+    assert.deepEqual(required(1), undefined)
+    assert.end()
+  })
 })
