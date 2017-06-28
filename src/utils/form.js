5569d422f1868742ebcb54425cc36735db8b5de7
David Hazeland <davidhazeland@gmail.com>
2017-06-28 17:27:56 +0700

    Update 'mapPath'

--- src/utils/form.js
+++ src/utils/form.js
@@ -3,5 +3,12 @@ import {mapValues, get} from 'lodash'
 export const notNegative = (value, previousValue) => value >= 0 ? value : previousValue
 
 export const mapPath = (source, map) => {
-  return mapValues(map, path => get(source, path))
+  return mapValues(map, value => {
+    if (typeof value === 'string') {
+      return get(source, value)
+    } else {
+      const {path, format} = value
+      return format(get(source, path))
+    }
+  })
 }
